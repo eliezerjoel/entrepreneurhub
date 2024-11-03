fef53d65fb5905a4df0645a2d929ee9af169b2d5
eliezerjoel <kagulirejoeleliezer@gmail.com>
2024-11-03 17:34:20 +0300

    desc

--- components/VentureCard.jsx
+++ components/VentureCard.jsx
@@ -6,6 +6,7 @@ const VentureCard = ({ venture }) => {
   const projectId = "66fd757200140d617b64";
   const imageUrl = `https://cloud.appwrite.io/v1/storage/buckets/${bucketId}/files/${venture.image}/view?project=${projectId}`;
   const imageSrc = venture.image ? imageUrl : "/images/no-image.jpg";
+  const desc = venture.description
 
   return (
     <div className="bg-white rounded-lg shadow-md overflow-hidden">
@@ -16,7 +17,7 @@ const VentureCard = ({ venture }) => {
       />
       <div className="p-4">
         <h2 className="text-xl font-bold">{venture.name}</h2>
-        <p className="text-gray-600">{venture.description}</p>
+        <p className="text-gray-600">{desc.substring(0, 100)}...</p>
         <Link href={`ventures/${venture.$id}`}>
           <button className="mt-4 bg-gray-800 text-white px-3 py-1 rounded hover:bg-gray-700">
             View Details
